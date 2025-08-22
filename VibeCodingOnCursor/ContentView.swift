0119f4a3c8813ac2c79750c532e1517c105289b3
devayz <fatokimiemman@gmail.com>
2025-08-22 11:09:42 +0100

    test Locale.current api - 16.0 target required

--- VibeCodingOnCursor/ContentView.swift
+++ VibeCodingOnCursor/ContentView.swift
@@ -8,12 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var amount = 12.0
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            
+            Text(amount, format: .currency(code: Locale.current.currency?.identifier ?? "$"))
+            
+            Text("Total: \(amount, format: .currency(code: Locale.current.currency?.identifier ?? "$"))")
         }
         .padding()
     }
