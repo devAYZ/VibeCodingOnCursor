29ff01d81d8997b311017962edc1ee2719106a01
devayz <fatokimiemman@gmail.com>
2025-08-22 10:49:56 +0100

    Initial Commit

---
+++ VibeCodingOnCursor/VibeCodingOnCursorApp.swift
@@ -0,0 +1,17 @@
+//
+//  VibeCodingOnCursorApp.swift
+//  VibeCodingOnCursor
+//
+//  Created by Ayokunle Fatokimi on 22/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct VibeCodingOnCursorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ VibeCodingOnCursorTests/VibeCodingOnCursorTests.swift
@@ -0,0 +1,17 @@
+//
+//  VibeCodingOnCursorTests.swift
+//  VibeCodingOnCursorTests
+//
+//  Created by Ayokunle Fatokimi on 22/08/2025.
+//
+
+import Testing
+@testable import VibeCodingOnCursor
+
+struct VibeCodingOnCursorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
